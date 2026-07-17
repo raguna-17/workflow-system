00610c1b3ccfd9896ed6e1a15729df63bf1300e4
らぐな <pasupoto17@gmail.com>
2026-07-17 09:25:35 +0900

    Initial commit

--- backend/src/main/resources/db/migration/V4__add_user_id_to_todos.sql
+++ backend/src/main/resources/db/migration/V4__add_user_id_to_todos.sql
@@ -1,8 +1,16 @@
-ALTER TABLE todos
-ADD COLUMN user_id BIGINT NOT NULL;
+DROP TABLE IF EXISTS todo CASCADE;
 
-ALTER TABLE todos
-ADD CONSTRAINT fk_todos_user
-FOREIGN KEY (user_id)
-REFERENCES users(id)
-ON DELETE CASCADE;
+CREATE TABLE todo (
+    id BIGSERIAL PRIMARY KEY,
+
+    title VARCHAR(255),
+
+    completed BOOLEAN NOT NULL,
+
+    user_id BIGINT NOT NULL,
+
+    CONSTRAINT fk_todo_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
